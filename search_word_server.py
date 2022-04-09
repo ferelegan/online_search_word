e0c62c2702aa6bf7563d7727d1c192dbb0f5c604
ferelegan <surpass1223@163.com>
2022-04-09 11:08:37 +0800

    初步框架

--- search_word_server.py
+++ search_word_server.py
@@ -1,12 +1,27 @@
 """
 在线查单词服务端
 """
-from multiprocessing import process
+from multiprocessing import Process
 from socket import *
 from pymysql import *
 
 class SearchWordTCP:
-    pass
+    def __init__(self):
+        self.__host = '0.0.0.0'
+        self.__port = '8888'
+        self.__address = (self.__host, self.__port)
+        self.__sock = self.__create_socket()
+
+    def __create_socket(self):
+        sock = socket()
+        sock.bind(self.__address)
+        return sock
+
+    def connect(self):
+        self.__sock.listen(5)
+        while True:
+            connfd,addr = self.__sock.accept()
+            swp = SearchWordProcess(connfd)
 
 class SearchWordSQL:
     def __init__(self):
@@ -28,10 +43,27 @@ class SearchWordSQL:
             print(item)
 
 class SearchWordHandle:
-    pass
+    def handle(self):
+        pass
 
-class SearchWordProcess(process):
-    pass
+class SearchWordProcess(Process):
+    def __init__(self,connfd):
+        super().__init__()
+        self.__connfd = connfd
+
+    def recv(self):
+        pass
+
+    def send(self):
+        pass
+
+    def handle(self):
+        handle = SearchWordHandle()
+        handle.handle()
+
+    def run(self) -> None:
+        pass
+
 
 
 if __name__ == '__main__':
